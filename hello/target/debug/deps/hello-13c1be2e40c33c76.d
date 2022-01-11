2b9f7315b9f6b3c9dda2804e7a738f9bb8a48b22
bartosz.ciesielczyk <ext-bartosz.ciesielczyk@here.com>
2022-01-11 16:00:01 +0100

    Programming Rust: Chapter 2 simple program

---
+++ hello/target/debug/deps/hello-13c1be2e40c33c76.d
@@ -0,0 +1,5 @@
+C:\Users\bartosz.ciesielczyk\Code\learning_rust\hello\target\debug\deps\hello-13c1be2e40c33c76.exe: src\main.rs
+
+C:\Users\bartosz.ciesielczyk\Code\learning_rust\hello\target\debug\deps\hello-13c1be2e40c33c76.d: src\main.rs
+
+src\main.rs:
