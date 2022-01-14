2d60a28a759b05d1ab6047713a065eff416be3fd
rzegnam <rzegnam@gmail.com>
2022-01-14 15:57:22 +0100

    deleted target folder

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,5 +1,26 @@
+use std::str::FromStr;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let mut numbers = Vec::new();
+
+    for arg in env::args().skip(1) {
+        numbers.push(u64::from_str(&arg)
+        .expect("error parsing argument"));
+    }
+
+    if numbers.len() == 0 {
+        eprintln!("Usage: gcd NUMBER ...");
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+
+    println!("The greatest common divisor of {:?} is {}", numbers, d);
+
 }
 
 fn gcd(mut n: u64, mut m: u64) -> u64 {
--- hello/target/debug/deps/hello-13c1be2e40c33c76.d
+++
@@ -1,5 +0,0 @@
-C:\Users\bartosz.ciesielczyk\Code\learning_rust\hello\target\debug\deps\hello-13c1be2e40c33c76.exe: src\main.rs
-
-C:\Users\bartosz.ciesielczyk\Code\learning_rust\hello\target\debug\deps\hello-13c1be2e40c33c76.d: src\main.rs
-
-src\main.rs:
