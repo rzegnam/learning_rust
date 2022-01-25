c64e22c3d30579cb27cf13534ee338b17d3c1440
rzegnam <rzegnam@gmail.com>
2022-01-25 08:44:46 +0100

    Variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,4 +3,6 @@ fn main() {
     println!("The value of x is {}", x);
     x = 6;
     println!("The value of x is {}", x);
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("The value of 3 hourds is {}", THREE_HOURS_IN_SECONDS);
 }
